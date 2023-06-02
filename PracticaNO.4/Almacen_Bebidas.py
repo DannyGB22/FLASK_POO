0767e3bc6d7cbbee14bea3cfb8659736e29f1531
DannyGB22 <125704277+DannyGB22@users.noreply.github.com>
2023-06-02 06:41:16 -0600

    Creacion del menu  y del controlador.
    En controlador se creo la clase bebida con sus respectivos atributos encapsulados , al igual se crearon los metodos getters y setters para poder acceder a nuestros atributos.

---
+++ PracticaNO.4/Almacen_Bebidas.py
@@ -0,0 +1,21 @@
+# from controlador import 
+
+print("Hola Bienvenido al almacen de bebidas".center(100,'-'))
+
+while True:
+    print("\nMENÚ".center(100,'='))
+    print("1. Agregar bebida")
+    print("2. Eliminar bebida")
+    print("3. Actualizar bebida")
+    print("4. Mostrar todas las bebidas")
+    print("5. Calcular precio promedio de las bebidas")
+    print("6. Cantidad de bebidas por marca")
+    print("7. Cantidad de bebidas por clasificación")
+    print("8. Salir")
+    
+
+seleccion = input('Ingrese el numero de la seleccion que desa hacer: ')
+
+
+
+    
---
+++ PracticaNO.4/controlador.py
@@ -0,0 +1,53 @@
+
+
+class Bebidas:
+    
+    def __init__(self, ID, nombre, precio, clasificacion, marca):
+        
+        self._ID = ID
+        self._nombre = nombre
+        self._precio = precio
+        self._clasificacion = clasificacion
+        self._marca = marca
+        
+#creacion de los metodos getter a setter
+@property
+def nombre(self):
+    return self._nomrbre
+
+@nombre.setter
+def nombre(self, nombre):
+    return self._nombre = nombre
+
+@property
+def precio(self):
+    return self._precio
+
+@precio.setter
+def precio(self, precio):
+    return self._precio = precio
+
+@property
+def clasificacion(self):
+    return self._clasificacion
+
+@clasificacion.setter
+def clasificacion(self, clasificacion):
+    return self._clasificacion = claificacion
+
+
+@property
+def marca(self):
+    return self._marca
+
+@marca.settter 
+def marca(self, marca):
+    return self._marca = marca
+
+
+
+
+
+
+      
+        
