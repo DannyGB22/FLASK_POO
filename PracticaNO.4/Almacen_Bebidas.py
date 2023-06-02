0970fac3c8504aadccd9359c36a702f706b9460f
DannyGB22 <125704277+DannyGB22@users.noreply.github.com>
2023-06-02 17:13:49 -0600

    Creacion de los metodos  y seleccion del menu.
    Se  crearon las funciones del crud  y los metodos para calcular promedio, cantidad  de bebidas por marca y clasificacion. Tambien se creo un if anidado para seleccionar los posibles casos del la accion a realizar de nuestro menu.

--- PracticaNO.4/Almacen_Bebidas.py
+++ PracticaNO.4/Almacen_Bebidas.py
@@ -1,9 +1,11 @@
-# from controlador import 
+from controlador import AlmacenBebidas
+
+almacen = AlmacenBebidas()
 
 print("Hola Bienvenido al almacen de bebidas".center(100,'-'))
 
 while True:
-    print("\nMENÚ".center(100,'='))
+    print("\n----------Menu-----------------------------------------------------------")
     print("1. Agregar bebida")
     print("2. Eliminar bebida")
     print("3. Actualizar bebida")
@@ -14,8 +16,32 @@ while True:
     print("8. Salir")
     
 
-seleccion = input('Ingrese el numero de la seleccion que desa hacer: ')
+    seleccion = input('\nIngrese el numero de la seleccion que desea hacer: ')
 
+    if seleccion == "1":
+     almacen.DarAlta()
+    elif seleccion == "2": 
+        id = int(input("Ingrese el ID de la bebida que desea eliminar"))
+        almacen.DarBaja(id)
+    elif seleccion == "3":
+        id = int(input("Ingrese el ID de la bebida que desea actualizar: "))
+        almacen.actualizar(id)
+    elif seleccion == "4":
+        almacen.Mostrar()
+    elif seleccion == "5":
+        almacen.PrecioPromedio()
+    elif seleccion == "6":
+        marca = input("Ingrese la marca de bebidas que desea buscar: ")
+        almacen.BebidaMarca(marca)
+    elif seleccion == "7":
+        clasificacion = input("Ingrese la clasificación de bebidas que desea buscar: ")
+        almacen.Clasificacion(clasificacion)
+    elif seleccion == "8":
+        break
+    else:
+        print("Seleccion invalida, agregue un numero  valido del Menu.")
+        
+        
 
 
     
--- PracticaNO.4/controlador.py
+++ PracticaNO.4/controlador.py
@@ -1,48 +1,139 @@
 
-
 class Bebidas:
     
-    def __init__(self, ID, nombre, precio, clasificacion, marca):
+    def __init__(self, id, nombre, precio, clasificacion, marca):
         
-        self._ID = ID
+        self._id = id
         self._nombre = nombre
         self._precio = precio
         self._clasificacion = clasificacion
         self._marca = marca
         
-#creacion de los metodos getter a setter
-@property
-def nombre(self):
-    return self._nomrbre
+    #creacion de los metodos getter a setter
+    
+    @property
+    def id(self):
+        return self._id
+    
+    
+    @property
+    def nombre(self):
+        return self._nombre
 
-@nombre.setter
-def nombre(self, nombre):
-    return self._nombre = nombre
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
 
-@property
-def precio(self):
-    return self._precio
+    @property
+    def precio(self):
+        return self._precio
 
-@precio.setter
-def precio(self, precio):
-    return self._precio = precio
+    @precio.setter
+    def precio(self, precio):
+        self._precio = precio
 
-@property
-def clasificacion(self):
-    return self._clasificacion
+    @property
+    def clasificacion(self):
+        return self._clasificacion
 
-@clasificacion.setter
-def clasificacion(self, clasificacion):
-    return self._clasificacion = claificacion
+    @clasificacion.setter
+    def clasificacion(self, clasificacion):
+        self._clasificacion = clasificacion
 
 
-@property
-def marca(self):
-    return self._marca
+    @property
+    def marca(self):
+        return self._marca
 
-@marca.settter 
-def marca(self, marca):
-    return self._marca = marca
+    @marca.setter 
+    def marca(self, marca):
+        self._marca = marca
+
+
+
+class AlmacenBebidas:
+    def __init__(self):
+        self.bebidas = []
+        
+        
+    def DarAlta(self):
+        
+        id = int(input("Ingrese el ID de la bebida: "))
+        nombre = input("Ingrese el nombre de la bebida: ")
+        precio = float(input("Ingrese el precio de la bebida: "))
+        clasificacion = input("Ingrese la clasificación de la bebida: ")
+        marca = input("Ingrese la marca de la bebida: ")
+    
+        bebida = Bebidas(id, nombre, clasificacion, marca, precio)
+        self.bebidas.append(bebida)
+        print("Bebida agregada correctamente.")
+        
+    
+    def DarBaja(self, id):
+        for bebida in self.bebidas:
+            if bebida.id == id:
+                self.bebidas.remove(bebida)
+                print("Se elimino correctamente la bebida.")
+                return
+        print("No se encontró ninguna bebida con el ID indicado")
+    
+    
+    def actualizar(self, id):
+        for bebida in self.bebidas:
+            if bebida.id == id:
+                nombre = input("Ingrese el nuevo nombre de la bebida: ")
+                precio = float(input("Ingrese el nuevo precio de la bebida: "))
+                clasificacion = input("Ingrese la nueva clasificación de la bebida: ")
+                marca = input("Ingrese la nueva marca de la bebida: ")
+                
+                bebida.nombre = nombre
+                bebida.precio = precio
+                bebida.clasificacion = clasificacion
+                bebida.marca = marca
+                print("Se actualizo correctamente la bebida.")
+                return
+        print("No se encontró ninguna bebida con el ID indicado.")
+        
+     
+    def Mostrar(self):
+        if len(self.bebidas) == 0:
+            print("No existen bebidas registradas.")
+        else:
+            for bebida in self.bebidas:
+                self.mostrarBebida(bebida) 
+       
+    def mostrarBebida(self, bebida):
+        print(f'ID: {bebida.id}')
+        print(f'Nombre: {bebida.nombre}')
+        print(f'Precio: {bebida.precio}')
+        print(f'Clasificacion: {bebida.clasificacion}')
+        print(f'Marca: {bebida.marca}')
+        print('----------------------------------------------------------------------------')
+        
+        
+    def PrecioPromedio(self):
+        if len(self.bebidas) == 0:
+            print("No existen bebidas registradas")
+        else:
+            total_precios = sum(bebida.precio for bebida in self.bebidas)
+            promedio = total_precios / len(self.bebidas)
+            print(f'El precio promedio de las bebidas es: {promedio}')
+            
+    def BebidaMarca(self, marca):
+        cantidad = sum(1 for bebida in self.bebidas if bebida.marca == marca)
+        print(f'Cantidad de bebidas de la marca {marca}: {cantidad}')
+    
+    
+    def Clasificacion(self, clasificacion):
+        cantidad = sum(1 for bebida in self.bebidas if bebida.clasificacion == clasificacion)
+        print(f'Cantidad de bebidas de la clasificación {clasificacion}: {cantidad}')
+
+
+
+        
+        
+        
+    
 
 
 
