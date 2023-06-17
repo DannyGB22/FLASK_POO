3417acbac2a00c7aed719914d08cd8ecdf234136
DannyGB22 <125704277+DannyGB22@users.noreply.github.com>
2023-06-16 18:49:06 -0600

    practicaNO6: vistas, envio informacion de vista a rutas.

--- PracticaNO.5/app.py
+++ PracticaNO.5/app.py
@@ -1,5 +1,4 @@
-from flask import Flask 
-
+from flask import Flask, render_template, request
 #Estamos declarando el app y le estamos asignando un nombre
 # Inicializacion del servidor flask
 app= Flask(__name__)
@@ -8,26 +7,28 @@ app.config['MYSQL_HOST']= "localhost"
 app.config['MYSQL_USER']= "root"
 app.config['MYSQL_PASSWORD']= "danny22"
 app.config['MYSQL_DB']= "dbflask"
-
-
 # Declaramos una ruta 
+# La ruta se compone del nombre y la funcion
 # Declaramos la ruta index http://localhost:5000
-# La ruta se compone del nombre y la funcion 
 
 @app.route('/')
 def index():
-    return "Hola Mundo"
+    return render_template('index.html')
 
 
-@app.route('/Guardar')
-def guadar():
-    return "Se guardo el album enla BD"
-
+@app.route('/guardar', methods=['POST'])
+def guardar():
+    if request.method== 'POST':
+        titulo= request.form['txtTitulo']
+        artista= request.form['txtArtista']
+        anio= request.form['txtAnio']
+        print(titulo, artista, anio)
+        
+    return "La info del album llego a su ruta amigo;"
 
 @app.route('/Eliminar')
 def eliminar():
     return "Se elimino el album en la BD"
-
 # Ejecucion 
 if __name__ == '__main__':
     app.run(port= 5000, debug = True)
